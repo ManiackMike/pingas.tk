91d029e9971b5f6622fd5e7fd7b2bbc5e19e9b0b
ManiackMike <105581753+ManiackMike@users.noreply.github.com>
2022-09-08 10:29:00 +0200

    Add files via upload

---
+++ MEME/Archivio_files/Archivio.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20220908
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Archivio_files/ArchivioMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');detectBrowser();Widget.onload();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id2');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
---
+++ MEME/MEME_files/MEME.js
@@ -0,0 +1,11 @@
+// Created by iWeb 3.0.2 local-build-20220908
+
+setTransparentGifURL('../Media/transparent.gif');function hostedOnDM()
+{return false;}
+function photocastSubscribe()
+{photocastHelper("file://localhost/Users/domiziolococo/Siti%20Web/Pingas/Pingas/MEME/rss.xml");}
+function onPageLoad()
+{loadMozillaCSS('MEME_files/MEMEMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');detectBrowser();adjustLineHeightIfTooBig('id4');adjustFontSizeIfTooBig('id4');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../Media/transparent.gif');fixupIECSS3Opacity('id3');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
