4a406c36e14f3b7b3c19d81db9c2c77c4fda4906
ManiackMike <105581753+ManiackMike@users.noreply.github.com>
2022-09-17 14:09:08 +0200

    s

--- MEME/Voci/2022/9/10_wenomechainsama_files/10_wenomechainsama.js
+++ MEME/Voci/2022/9/10_wenomechainsama_files/10_wenomechainsama.js
@@ -1,4 +1,4 @@
-// Created by iWeb 3.0.2 local-build-20220910
+// Created by iWeb 3.0.2 local-build-20220911
 
 function writeMovie1()
 {detectBrowser();if(windowsInternetExplorer)
@@ -11,6 +11,6 @@ setTransparentGifURL('../../../../Media/transparent.gif');function hostedOnDM()
 {return false;}
 function onPageLoad()
 {dynamicallyPopulate();loadMozillaCSS('10_wenomechainsama_files/10_wenomechainsamaMoz.css')
-adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id4');BlogFixupPreviousNext();performPostEffectsFixups()}
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id5');adjustFontSizeIfTooBig('id5');adjustLineHeightIfTooBig('id6');adjustFontSizeIfTooBig('id6');Widget.onload();fixAllIEPNGs('../../../../Media/transparent.gif');fixupIECSS3Opacity('id4');BlogFixupPreviousNext();performPostEffectsFixups()}
 function onPageUnload()
 {Widget.onunload();}
---
+++ Pingas_Man_Festival_files/Pingas_Man_Festival.js
@@ -0,0 +1,9 @@
+// Created by iWeb 3.0.2 local-build-20220917
+
+setTransparentGifURL('Media/transparent.gif');function hostedOnDM()
+{return false;}
+function onPageLoad()
+{loadMozillaCSS('Pingas_Man_Festival_files/Pingas_Man_FestivalMoz.css')
+adjustLineHeightIfTooBig('id1');adjustFontSizeIfTooBig('id1');adjustLineHeightIfTooBig('id2');adjustFontSizeIfTooBig('id2');adjustLineHeightIfTooBig('id3');adjustFontSizeIfTooBig('id3');Widget.onload();fixupAllIEPNGBGs();fixAllIEPNGs('Media/transparent.gif');fixupIECSS3Opacity('id4');performPostEffectsFixups()}
+function onPageUnload()
+{Widget.onunload();}
